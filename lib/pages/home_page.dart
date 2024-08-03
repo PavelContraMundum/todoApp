8d17619545216d9492a5fb33ac7223887d99fb33
PavelContraMundum <pavel.sticka@gmail.com>
2024-08-03 15:38:44 +0200

    checkbox, styly

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of todo tasks
+
+  List toDoList = [
+    ["Make coffee", false],
+    ["Do Exercise", false],
+  ];
+
+  //checkbox was tapped
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +32,15 @@ class _HomePageState extends State<HomePage> {
         title: Text('TO DO'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [TodoTile()],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
