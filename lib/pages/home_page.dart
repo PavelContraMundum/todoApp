98ea85b7c1349821bea03405c595a62ee8fed7b1
PavelContraMundum <pavel.sticka@gmail.com>
2024-08-03 14:35:56 +0200

    prvni commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todo/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('TO DO'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [TodoTile()],
+      ),
+    );
+  }
+}
---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  const TodoTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Make coffee"),
+    );
+  }
+}
